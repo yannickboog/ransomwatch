71603c125b00ef27753cf5e327b5cf623e7f3ff4
Yannick Boog <3501967+yannickboog@users.noreply.github.com>
2025-06-30 16:05:38 +0200

    Add author and description info

---
+++ ransomwatch.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""
+ransomwatch - Ransomware Intelligence Tool
+Designed for threat intelligence, security research, and situational awareness.
+
+Author: Yannick Boog
+"""
+
